d75650c4e96a5f692ede8e0bafbcb324d6d75e62
Sumon <Sumon@DESKTOP-1B5EHLS>
2020-03-01 22:19:09 -0500

    Just Added one headings:

--- src/main/java/com/piit/bootcamp/homedepot/Home_P1.java
+++ src/main/java/com/piit/bootcamp/homedepot/Home_P1.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 public class Home_P1 {
+	
+	//Added this project into Github and imported from github and now working on it::--->>>
+	
 	WebDriver driver;
 	@FindBy(id="headerMyAccount")
 	WebElement MyAccount;
